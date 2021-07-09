d7b8333e0f888719f2de47d4197cb05225b2aafc
James Strachan <james.strachan@gmail.com>
2021-07-09 15:11:42 +0100

    fix: add a BDD test

--- .github/workflows/bdd.sh
+++ .github/workflows/bdd.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
 
-echo "running the BDD tests"
-./kind.sh create
+
+
+export BDD_NAME="kind"
+export BRANCH_NAME="${BRANCH_NAME:-pr-${GITHUB_RUN_ID}-${GITHUB_RUN_NUMBER}}"
+export BUILD_NUMBER="${GITHUB_RUN_NUMBER}"
+
+export CLUSTER_NAME="${BRANCH_NAME,,}-$BUILD_NUMBER-$BDD_NAME"
+
+jx scm repo create https://github.com/${GIT_OWNER}/cluster-$CLUSTER_NAME --template https://github.com/jx3-gitops-repositories/jx3-kind --private --confirm
+sleep 15
+jx scm repo clone https://github.com/${GIT_OWNER}/cluster-$CLUSTER_NAME cluster-dev
+
+pushd `pwd`/cluster-dev
+    echo "creating the kind cluster"
+    ./kind.sh create
+
+    echo "running the BDD tests"
+    ./run_bdd.sh
+popd
+
+
